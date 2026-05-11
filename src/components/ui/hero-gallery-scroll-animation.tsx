eeb624cdeac7b9a6debbe25c45e9af3790d5ffcb
Nikola <dzonifu@gmail.com>
2026-05-11 11:29:09 +0200

    feat(ui): scroll-driven Motion primitives (ContainerScroll/BentoGrid/BentoCell/ContainerScale)
    Reusable scroll-tied animacije iz template-a, prilagođene za projekt
    (cn iz @/lib/utils/cn, uklonjeno YAGNI varijante).

---
+++ src/components/ui/hero-gallery-scroll-animation.tsx
@@ -0,0 +1,114 @@
+"use client";
+
+import * as React from "react";
+import { cva, type VariantProps } from "class-variance-authority";
+import {
+  HTMLMotionProps,
+  MotionValue,
+  motion,
+  useScroll,
+  useTransform,
+} from "motion/react";
+
+import { cn } from "@/lib/utils/cn";
+
+const bentoGridVariants = cva(
+  "relative grid gap-4 [&>*:first-child]:origin-top-right [&>*:nth-child(3)]:origin-bottom-right [&>*:nth-child(4)]:origin-top-right",
+  {
+    variants: {
+      variant: {
+        default: `
+          grid-cols-8 grid-rows-[1fr_0.5fr_0.5fr_1fr]
+          [&>*:first-child]:col-span-8 md:[&>*:first-child]:col-span-6 [&>*:first-child]:row-span-3
+          [&>*:nth-child(2)]:col-span-2 md:[&>*:nth-child(2)]:row-span-2 [&>*:nth-child(2)]:hidden md:[&>*:nth-child(2)]:block
+          [&>*:nth-child(3)]:col-span-2 md:[&>*:nth-child(3)]:row-span-2 [&>*:nth-child(3)]:hidden md:[&>*:nth-child(3)]:block
+          [&>*:nth-child(4)]:col-span-4 md:[&>*:nth-child(4)]:col-span-3
+          [&>*:nth-child(5)]:col-span-4 md:[&>*:nth-child(5)]:col-span-3
+        `,
+      },
+    },
+    defaultVariants: { variant: "default" },
+  },
+);
+
+type ContainerScrollContextValue = { scrollYProgress: MotionValue<number> };
+const ContainerScrollContext = React.createContext<
+  ContainerScrollContextValue | undefined
+>(undefined);
+
+function useContainerScrollContext() {
+  const ctx = React.useContext(ContainerScrollContext);
+  if (!ctx) {
+    throw new Error("useContainerScrollContext must be used within ContainerScroll");
+  }
+  return ctx;
+}
+
+export function ContainerScroll({
+  children,
+  className,
+  ...props
+}: React.HTMLAttributes<HTMLDivElement>) {
+  const scrollRef = React.useRef<HTMLDivElement>(null);
+  const { scrollYProgress } = useScroll({ target: scrollRef });
+  return (
+    <ContainerScrollContext.Provider value={{ scrollYProgress }}>
+      <div
+        ref={scrollRef}
+        className={cn("relative min-h-screen w-full", className)}
+        {...props}
+      >
+        {children}
+      </div>
+    </ContainerScrollContext.Provider>
+  );
+}
+
+export const BentoGrid = React.forwardRef<
+  HTMLDivElement,
+  React.HTMLAttributes<HTMLDivElement> & VariantProps<typeof bentoGridVariants>
+>(({ variant, className, ...props }, ref) => (
+  <div
+    ref={ref}
+    className={cn(bentoGridVariants({ variant }), className)}
+    {...props}
+  />
+));
+BentoGrid.displayName = "BentoGrid";
+
+export const BentoCell = React.forwardRef<HTMLDivElement, HTMLMotionProps<"div">>(
+  ({ className, style, ...props }, ref) => {
+    const { scrollYProgress } = useContainerScrollContext();
+    const translate = useTransform(scrollYProgress, [0.1, 0.9], ["-35%", "0%"]);
+    const scale = useTransform(scrollYProgress, [0, 0.9], [0.5, 1]);
+    return (
+      <motion.div
+        ref={ref}
+        className={className}
+        style={{ translate, scale, ...style }}
+        {...props}
+      />
+    );
+  },
+);
+BentoCell.displayName = "BentoCell";
+
+export const ContainerScale = React.forwardRef<HTMLDivElement, HTMLMotionProps<"div">>(
+  ({ className, style, ...props }, ref) => {
+    const { scrollYProgress } = useContainerScrollContext();
+    const opacity = useTransform(scrollYProgress, [0, 0.5], [1, 0]);
+    const scale = useTransform(scrollYProgress, [0, 0.5], [1, 0]);
+    const position = useTransform(scrollYProgress, (p) =>
+      p >= 0.6 ? "absolute" : "fixed",
+    );
+    return (
+      <motion.div
+        ref={ref}
+        className={cn("left-1/2 top-1/2 size-fit", className)}
+        style={{ translate: "-50% -50%", scale, position, opacity, ...style }}
+        {...props}
+      />
+    );
+  },
+);
+ContainerScale.displayName = "ContainerScale";
